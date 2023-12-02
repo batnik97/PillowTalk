d30407abdb930a54bcc4e0d15ed8434a00cead48
Vrushank Agrawal <vrushank2001@gmail.com>
2023-12-02 17:54:36 -0500

    add docker command

--- src/server.js
+++ src/server.js
@@ -8,8 +8,8 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Hello, World!\n');
-  exec('./llama.cpp/main -m ./llama.cpp/zephyr-7b-beta.Q5_K_M.gguf -n 256 --repeat_penalty 1.0 --color -r "User:" -f ./llama.cpp/prompts/alpaca.txt', (err, stdout, stderr) => {
+  exec('docker run -it -v /Users/nikhilreddy/OSSHack/spx-data:/data --rm msftspeech/spx recognize --file record_out.wav > test.txt');
+  exec('./llama.cpp/main -m ./llama.cpp/zephyr-7b-beta.Q5_K_M.gguf -n 256 --repeat_penalty 1.0 --color -r "User:" -f ./data/alpaca.txt', (err, stdout, stderr) => {
     if (err) {
       console.log(err);
       return;
---
+++ src/stt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <socket.h>
+
+    // docker run -it -v /Users/nikhilreddy/OSSHack/spx-data:/data --rm msftspeech/spx recognize --file record_out.wav > test.txt
+
+int main(argc, argv) {
+
+    // create a server socket
+    // bind the socket to a port
+    // listen for connections
+
+    while (true) {
+
+
+    }
+}
